c933eb2948e43a2ca2b76401d573f12a1cc2244a
Sepehr Fazeli <sepehr88258@gmail.com>
2022-03-09 19:51:10 +0330

    version 0.1.2

--- target/generated-sources/annotations/ir/mapsa/jobinja/mapper/ReservedListMapperImpl.java
+++ target/generated-sources/annotations/ir/mapsa/jobinja/mapper/ReservedListMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-04T11:52:07+0330",
+    date = "2022-03-09T18:52:53+0330",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
